cd64fcfd567b594304d6ceb648731c288e9e63dd
Nada Hosny <nadahosni96@gmail.com>
2022-09-08 03:59:14 -0700

    fixed my code according to the betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
-int main()
+
+/*
+ * main function is our entry point here
+ *
+ * Description: printing a line using puts function
+ *
+ * returns 0
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"); 	                
- 	return 0;
-}              
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
