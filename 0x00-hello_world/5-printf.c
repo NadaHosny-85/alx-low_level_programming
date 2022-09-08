f20da830b9c82391c6ad36bca59390987865cd25
Nada Hosny <nadahosni96@gmail.com>
2022-09-08 04:25:10 -0700

    added a c file that contains printing using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+ * Descriotion: this code prints a sentence using the printf
+ */
+
+/**
+ * main - the main contains our printf statement
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
