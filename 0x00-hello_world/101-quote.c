47de1544e0e8cfd9664d46b8314a8397956acff0
Nada Hosny <nadahosni96@gmail.com>
2022-09-08 06:15:41 -0700

    updated c file to match task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,8 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, str, strlen(str));
-	return(1);
+	return (1);
 }
