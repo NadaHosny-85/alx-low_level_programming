39d2f4bee786a8dee04299fc151bf4a75126dbc2
Nada Hosny <nadahosni96@gmail.com>
2022-09-09 04:21:50 -0700

    added a c file that detects variable is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/*
+ * Description: this mini program detects number
+ * is positive or negative or zero
+ */
+
+/**
+ * main - function contains if statement
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
