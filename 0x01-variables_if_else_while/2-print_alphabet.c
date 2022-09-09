88aaf7b6c167b854ffa1af0c2873de7d3366f345
Nada Hosny <nadahosni96@gmail.com>
2022-09-09 14:52:24 -0700

    added a c file that prints alphabets in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/*
+ * Description: This project prints alphabet
+ * from a to z in lower case using putchar
+ */
+/**
+ * main - function contains the loop and print
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch == 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
