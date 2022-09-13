3321e4abeb5e58f9212690b385358c27d6959415
Nada Hosny <nadahosni96@gmail.com>
2022-09-13 14:06:55 -0700

    added a c file that returns absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+ * description: this program
+ * computes absolute value of num
+ */
+/**
+ * _abs - function that returns
+ * absolute value of integer
+ *
+ * @int: integer
+ *
+ * Return: integer value of number
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ char _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 
 #endif
