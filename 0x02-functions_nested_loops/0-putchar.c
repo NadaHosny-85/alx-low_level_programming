22e955f82af5ce60e268559c2da06a7486681d77
Nada Hosny <nadahosni96@gmail.com>
2022-09-13 07:49:12 -0700

    updated c and header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 /*
  * Description: this program prints a string
  * using putchar
  */
 /**
- * main - printing the string
+ * PrintStr - printing the string
  *
  * Return: always 0
  */
-int main(void)
+char PrintStr(void)
 {
-	PrintStr();
+	char str[] = "_putchar";
+
+	unsigned int i;
+
+	for (i = 0; i <= strlen(str); i++)
+	{
+		if (str[i] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+		{
+			putchar(str[i]);
+		}
+	}
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,47 +1,7 @@
-#include <stdio.h>
-#include <string.h>
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * PrintStr - function prints a string
- * using putchar function
- *
- * Return: always 0
- */
-int PrintStr(void)
-{
-	char str[] = "_putchar";
 
-	unsigned int i;
+char PrintStr(void);
+void print_alphabet(void);
 
-	for (i = 0; i <= strlen(str); i++)
-	{
-		if (str[i] == '\0')
-		{
-			putchar('\n');
-			break;
-		}
-		else
-		{
-			putchar(str[i]);
-		}
-	}
-	return (0);
-}
-
-/**
- * print_alphabet - prints alphabets in lower case
- *
- * Return: always 0
- */
-void print_alphabet(void)
-{
-	char alpha;
-
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
-		putchar(alpha);
-	}
-	putchar('\n');
-}
 #endif
