a1dfcd1adab171dd14e3aee2b9a4d8b3c5af7d8f
Nada Hosny <nadahosni96@gmail.com>
2022-09-14 03:29:21 -0700

    added a file that returns addition of two nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * description - program adds
+ * two integers and returns the result
+ */
+/**
+ * add - functions adds ints and returns
+ * result
+ *
+ * @a : first parameter
+ * @b : second parameter
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
