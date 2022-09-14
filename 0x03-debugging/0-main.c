189777b06f370420e0d6c1749513327160d4094d
Nada Hosny <nadahosni96@gmail.com>
2022-09-14 05:59:16 -0700

    added a main file for testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * description - this file
+ * contains our main function
+ * which tests out positive or negative function
+ */
+/**
+ * main - function has the tests
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	positive_or_negative(16);
+	return (0);
+}
