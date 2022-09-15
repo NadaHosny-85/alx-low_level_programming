0fbfff00aea72c868325cd9b4e9e7c3c63fb6eb4
Nada Hosny <nadahosni96@gmail.com>
2022-09-15 07:07:13 -0700

    added a c file that prints square of #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "mainh"
+/*
+ * description - program prints
+ * triangles
+ */
+/**
+ * print_triangle - function prints triangle
+ * of # through the input size
+ *
+ * @size: first parameter
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	if (size == 0 || size < 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= size; i++)
+		{
+			int j;
+
+			for (j = i; j < size; j++)
+			{
+				putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
