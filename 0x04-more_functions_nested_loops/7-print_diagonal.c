f576f4b2702468b89f04a911ac4e8c2408b96c74
Nada Hosny <nadahosni96@gmail.com>
2022-09-15 04:45:14 -0700

    added a c file that prints a diagonal straight line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * description - this program prints diagonal
+ *
+ */
+/**
+ * print_line - function prints
+ * diagonal straight line
+ *
+ * @n: parameter
+ * which indicates how many lines
+ *
+ * Return: 0
+ */
+vod print_line(int n)
+{
+	if (n == 0 || n < 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			for (j = 0; j <= i; j++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+}
