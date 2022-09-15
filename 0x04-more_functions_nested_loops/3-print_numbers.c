d19220d1d55c867ecd594469764afd2870bfe7f5
Nada Hosny <nadahosni96@gmail.com>
2022-09-15 01:35:10 -0700

    added a file that prints nums from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * descriptio - program prints
+ * numbers from 0 to 9
+ */
+/**
+ * print_numbers - function prints numbers
+ * through loop
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
