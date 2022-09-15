a533e921064cd6e55e445cffcf7c842603e8e592
Nada Hosny <nadahosni96@gmail.com>
2022-09-15 01:30:25 -0700

    added a file that maultiplies integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * description - program multiplies digits
+ */
+/**
+ * mul - function multiplies two
+ * integers
+ *
+ * @a : first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
