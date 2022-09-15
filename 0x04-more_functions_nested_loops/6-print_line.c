090c8414dad7821f06f11e9506fa15f73b390259
Nada Hosny <nadahosni96@gmail.com>
2022-09-15 02:48:10 -0700

    added a c file that prints straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * description - prints a straight line
+ */
+/**
+ * print_line - prints lines based on input
+ *
+ * @n: first parameter
+ * which determines how long is the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0 || n < 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+	}
+}
