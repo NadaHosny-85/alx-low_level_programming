3b38bf8fc9197aafbb423b7d9c1fa5babea30d98
Nada Hosny <nadahosni96@gmail.com>
2022-09-15 06:17:55 -0700

    added a c file that prints program Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/*
+ * description - Fizz Buzz program
+ */
+/**
+ * main - function prints nums from 1 to 100
+ * if num is multiple of 3 prints Fizz
+ * if num is multiple of 5 prints Buzz
+ * if num is multiple of both prints FizzBuzz
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
