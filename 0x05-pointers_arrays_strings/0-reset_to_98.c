435bf11a5de5e765590d0af5507128410810be1d
Nada Hosny <nadahosni96@gmail.com>
2022-09-19 03:41:46 -0700

    created a new directory, added README file and a new c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * description: short program for
+ * setting a pointer
+ */
+/**
+ * reset_to_98 - function sets pointer
+ * to num 98
+ *
+ * @n: parameter pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This folder contains short programs for arrays, strings and pointers related to
+ALX software engineering program
