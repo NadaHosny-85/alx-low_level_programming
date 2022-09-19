58f8e35b08a710e2c57ec2b3eaf65446982b8b37
Nada Hosny <nadahosni96@gmail.com>
2022-09-19 03:50:46 -0700

    added a c file that swaps two ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * description - program swaps to numbers
+ */
+/**
+ * swap_int - function swaps two
+ * number
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *tmp = &a;
+
+	*a = &b;
+
+	*b = &tmp;
+
+}
