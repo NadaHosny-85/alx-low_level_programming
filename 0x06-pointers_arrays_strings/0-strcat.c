3b08be87a394130b4b7be873d70cf6838eb6e0cd
Nada Hosny <nadahosni96@gmail.com>
2022-09-21 02:03:58 -0700

    added a c file that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <<string.h>
+#include "main.h"
+
+/*
+ * description - program concatenates
+ * input strings
+ */
+/**
+ * *_strcat - funcion concatenates  input
+ * string parameters
+ *
+ * @dest: first parameter
+ *
+ * @src: second parameter
+ *
+ * Return : the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int end_dest = 0;
+
+	int i = 0;
+
+	while (*dest[end_dest] != '\0')
+	{
+		end_dest++;
+	}
+	while (*src[i] != '\0')
+	{
+		dest[end_dest] = src[i];
+		end_dest++;
+		i++;
+	}
+	return (*dest);
+}
+
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains more mini programs for basic functions of programming as per the tasks needed in the ALX software engineering program
