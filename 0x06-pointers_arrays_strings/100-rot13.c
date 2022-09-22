3655b3432a0c266163a862b1369795220bb6e4e4
Nada Hosny <nadahosni96@gmail.com>
2022-09-22 16:41:24 -0700

    added a c file that encodes alphabets by rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * description - program encodes
+ * alphabet with rot13
+ */
+/**
+ * *rot13 - function shifts every letter
+ * of the given string by 13 letters
+ *
+ * @str: first parameter
+ * 
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	char alpha[] = { 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D',
+		'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I',
+		'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N',
+		'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S',
+		't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X',
+		'y', 'Y', 'z', 'Z'};
+	
+	char encoded_alpha[] = {'n', 'N', 'o', 'O', 'p', 'P',
+		'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U',
+		'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z',
+		'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E',
+		'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J',
+		'k', 'K', 'l', 'L', 'm', 'M'};
+
+	int i;
+
+	int j;
+
+	int size = sizeof(alpha) / sizeof(alpha[0]);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = encoded_alpha[j];
+			}
+		}
+	}
+	return (str);
+}
+
+
