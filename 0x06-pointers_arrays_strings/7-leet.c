2b543d28a9e092b08cba1faeb5e146f389faf556
Nada Hosny <nadahosni96@gmail.com>
2022-09-22 07:37:55 -0700

    added a file that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * description - program
+ * encodes alphabets into numbers
+ */
+/**
+ * *leet - function encodes some
+ * of the alphabets in a string
+ *
+ * @str: first parameter
+ *
+ * Return: a string encoded
+ */
+char *leet(char *str)
+{
+	char chars[] = {'a', 'A', 'e', 'E', 'o', 'O',
+		't', 'T', 'l', 'L'};
+
+	char encoded_chars[] = {'4', '4', '3', '3', '0', '0',
+		'7', '7', '1', '1'};
+
+	int i;
+
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; chars[j] != '\0'; j++)
+		{
+			if (str[i] == chars[j])
+			{
+				str[i] = encoded_chars[j];
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	return (str);
+}
