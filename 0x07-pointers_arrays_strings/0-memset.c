6b73851df8e6de4f98dae5c6b2141255a85888dd
Nada Hosny <nadahosni96@gmail.com>
2022-09-26 01:52:58 -0700

    added a c file that fills bytes with a constant char

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * description - program fills space
+ * with a constant byte
+ */
+/**
+ * *_memset - function fills specific num of bytes
+ * with a constant char
+ *
+ * @s: first array parameter
+ * @b: constant byte to fill in
+ * @n: number of bytes to fill
+ *
+ * Return: array of characters
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
