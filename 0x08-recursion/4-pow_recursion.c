72c706fafc4921248f8c333778625c7beb1299aa
Nada Hosny <nadahosni96@gmail.com>
2022-09-28 07:35:19 -0700

    added file for calculation

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * description - program returns calculation of
+ * num to power of another
+ */
+/**
+ * _pow_recursion - function returns num to power of
+ * another using recursion
+ *
+ * @x: base number
+ * @y: power
+ *
+ * Return: calculation of int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x > 0)
+	{
+		return (-1);
+	}
+	else if (x == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		y--;
+		return (_pow_recursion(x, y) * x);
+	}
+}
