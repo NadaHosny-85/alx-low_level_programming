fc549f61d6f6ea3cf928e375da9933ea3439f0c4
Nada Hosny <nadahosni96@gmail.com>
2022-09-28 03:20:28 -0700

    added a c file that prints string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * description - program uses recursion to print
+ * a string
+ */
+/**
+ * _puts_recursion - function prints
+ * a string using recursion
+ *
+ * @s: first parameter
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
