81b34ed3fd717ab3eaf20b337cf1ded6c20653bf
Nada Hosny <nadahosni96@gmail.com>
2022-09-30 14:51:55 -0700

    added a c file that prints no of arguments passed to main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * description - program prints
+ * number of arguments passed to it
+ */
+/**
+ * main - function prints no of args
+ *
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	argc--;
+	printf("%d\n", argc);
+	return (0);
+}
