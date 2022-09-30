e54c936aa510f1b46f9fba0c7c977c6da95d823b
Nada Hosny <nadahosni96@gmail.com>
2022-09-30 11:35:11 -0700

    added a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * description - program
+ * prints its name
+ */
+/**
+ * main - function prints its
+ * name
+ *
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+This directory contains introductory programs of argc and argv which
+is part of ALX software engineering program
