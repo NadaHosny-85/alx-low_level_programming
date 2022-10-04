9dd871018acc4fe1febcf54beeae20a0f3a047c9
Nada Hosny <nadahosni96@gmail.com>
2022-10-04 13:11:17 -0700

    fixed betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	arr_2d = malloc(sizeof(int*) * height);
+	arr_2d = malloc(sizeof(int) * height);
 	for (i = 0; i < height; i++)
 	{
 		arr_2d[i] = malloc(sizeof(int) * width);
