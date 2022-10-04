4c765ea833929d6e48ecf678687967bdca281f44
Nada Hosny <nadahosni96@gmail.com>
2022-10-04 13:08:03 -0700

    added a c file that allocates memory for 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/*
+ * description - program
+ * allocated memory for a 2d array
+ */
+/**
+ * **alloc_grid - function allocates
+ * memory for a 2-dimentional array
+ *
+ * @width: first parameter
+ * @height: second parameter
+ *
+ * Return: NULL if failed
+ * or 2d arr
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr_2d;
+
+	int i;
+
+	int j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	arr_2d = malloc(sizeof(int*) * height);
+	for (i = 0; i < height; i++)
+	{
+		arr_2d[i] = malloc(sizeof(int) * width);
+		if (arr_2d[i] == 0)
+		{
+			for (j = 0; j < width; j++)
+			{
+				free(arr_2d[j]);
+			}
+			free(arr_2d);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			arr_2d[i][j] = 0;
+		}
+	}
+	return (arr_2d);
+}
