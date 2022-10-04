6939a084f4fc18b1d96e7f6ac91eb031e72cc99d
Nada Hosny <nadahosni96@gmail.com>
2022-10-04 05:47:39 -0700

    added a c file that allocates space for concatenated strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdib.h>
+#include "main.h"
+/*
+ * description - program returns
+ * space allocated for 2 strings
+ * concatenated
+ */
+/**
+ * *str_concat - function concatenates
+ * two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: NULL on failure or
+ * concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str_concat = NULL;
+
+	unsigned int i = 0;
+
+	int j = 0;
+
+	int size1 = 0;
+
+	int size2 = 0;
+
+	if (s1 == NULL)
+	{s1 = ""; }
+	if (s2 == NULL)
+	{s2 = ""; }
+	while (s1[size1] != '\0')
+	{size1++; }
+	while (s2[size2] != '\0')
+	{size2++; }
+	strconcat = malloc(sizeof(char) * (size1 + size2) + 1);
+	if (str_concat == NULL)
+	{return (NULL); }
+	while (s1[i] != '\0')
+	{str_concat[i] = s1[i];
+		i++; }
+	while (s2[j] != '\0')
+	{str_concat[i] = s2[j];
+		j++;
+		i++; }
+	return (str_concat);
+}
