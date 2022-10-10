a4039f3e2e455ae86891ff33f57b265f280a6e1e
Nada Hosny <nadahosni96@gmail.com>
2022-10-10 06:32:53 -0700

    added a c file that initializes a structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+/*
+ * description - program
+ * initializes a var of type
+ * struct dog
+ */
+/**
+ * init_dog - function initializes
+ * a structure of a dog
+ *
+ * @d: first parameter
+ * @name: second parameter
+ * @age: third parameter
+ * @owner: fourth parameter
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
