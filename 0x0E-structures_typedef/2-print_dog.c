39c53a07df0423f98235a8218c68bd7007900fa9
Nada Hosny <nadahosni96@gmail.com>
2022-10-10 06:53:17 -0700

    added a c file that prints struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "dog.h"
+/*
+ * description - program
+ * prints a struct
+ */
+/**
+ * print_dog - function prints
+ * a struct of dog
+ *
+ * @d: first parameter
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age == NULL)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
