934b2df3faf62c9cc589a437380807a75e39a011
Nada Hosny <nadahosni96@gmail.com>
2022-10-20 15:42:32 -0700

    added an assembly file that prints hello

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,26 @@
+extern printf ;so it could be used
+
+;program prints "Hello, Holberton"
+;according to ALX software engineering program task
+;it should be a 64bit program
+
+;here comes the data section
+SECTION.data ;initializing what will we print
+
+msgToPrint: db "Hello, Holberton", 0	;the print
+msgLength: db "%s", 10, 0		;length of string
+
+;here comes the code section
+SECTION.text
+
+glabal main
+main:
+push rbp
+
+mov rdi,msgLength
+mov rsi,msgToPrint
+mov rax,0
+call printf
+pop rbp
+mov rax,0
+ret
