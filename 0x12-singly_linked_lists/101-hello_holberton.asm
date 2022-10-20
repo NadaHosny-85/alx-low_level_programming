a4d254bd942eb7edf8c8b35bf698f4c54dfdbf5e
Nada Hosny <nadahosni96@gmail.com>
2022-10-20 15:48:05 -0700

    added a fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,3 @@
-extern printf ;so it could be used
-
 ;program prints "Hello, Holberton"
 ;according to ALX software engineering program task
 ;it should be a 64bit program
@@ -13,14 +11,18 @@ msgLength: db "%s", 10, 0		;length of string
 ;here comes the code section
 SECTION.text
 
+extern printf ;so it could be used 
 glabal main
 main:
 push rbp
+mov rbp
+mov rsp
 
 mov rdi,msgLength
 mov rsi,msgToPrint
 mov rax,0
 call printf
+
 pop rbp
 mov rax,0
 ret
