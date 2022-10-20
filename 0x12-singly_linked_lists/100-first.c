daca6e909deacadc6b0ebcc908b7c904f9ae728c
Nada Hosny <nadahosni96@gmail.com>
2022-10-20 15:06:26 -0700

    added a function that prints before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * description - program will execute
+ * before main
+ */
+void before_main() __attribute__((constructor));
+
+/**
+ * before_main - function executes before
+ * main to print a sentence
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
