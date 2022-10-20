3368586aebb685c4970b31bdda247ff44d38b554
Nada Hosny <nadahosni96@gmail.com>
2022-10-20 15:13:40 -0700

    fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,13 @@
  * description - program will execute
  * before main
  */
-void before_main() __attribute__((constructor));
+void __attribute__((constructor)) before_main(void);
 
 /**
  * before_main - function executes before
  * main to print a sentence
  */
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,
 			\nI bore my house upon my back!\n");
