80cc0c8de407fd2e382c8eb74586692da2494d7d
Nada Hosny <nadahosni96@gmail.com>
2022-10-25 12:20:07 -0700

    added a file that returns nth node of list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/*
+ * description - program checks if node
+ * exists inside list
+ */
+/**
+ * *get_nodeint_at_index - function searches
+ * for node  inside a linked list
+ *
+ * @head: first parameter
+ * @index: second parameter
+ *
+ * Return: nth node of linked list
+ * NULL if it doesnt exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t n = 0;
+
+	while ((n < index) && (head->next != NULL))
+	{
+		head = head->next;
+		n++;
+	}
+	if (n < index)
+	{
+		return (NULL);
+	}
+	return (head)
+}
