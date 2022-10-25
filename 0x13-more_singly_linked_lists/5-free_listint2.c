0427187eff03dc3d1ddefda0f2b7f9565a61d8f3
Nada Hosny <nadahosni96@gmail.com>
2022-10-25 11:24:28 -0700

    added a file that frees a linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/*
+ * description - program frees
+ * a list
+ */
+/**
+ * free_listint2 - function frees
+ * linked list
+ *
+ * @head: parameter
+ * 
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp_node;
+
+	while (*head != NULL)
+	{
+		tmp_node = *head;
+		*head = (*head)->next;
+		free(tmp_node);
+	}
+	head = NULL;
+}
