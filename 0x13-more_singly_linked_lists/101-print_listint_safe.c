7fe6f1f45abb6b26be863b95ff29014fdbe4f477
Nada Hosny <nadahosni96@gmail.com>
2022-10-25 14:47:12 -0700

    added a file that prints nodes

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/*
+ * description - program prints
+ * list
+ */
+/**
+ * print_listint_safe - function prints
+ * linked list
+ *
+ * @head: first parameter
+ *
+ * Return: number of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tmp_node = head;
+
+	size_t i = 0;
+
+	if (head == NULL)
+	{
+		return (98);
+	}
+	while (head != NULL)
+	{
+		printf("[%p] %i\n",(void *)tmp_node, tmp_node->n);
+		tmp_node = tmp_node->next;
+		i++;
+	}
+	return (i);
+}
