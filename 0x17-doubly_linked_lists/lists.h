d4e9e62923bbc6c435463de84cdebd24d90a041a
Nada Hosny <nadahosni96@gmail.com>
2022-12-09 07:22:17 -0800

    changed name to fit task

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -5,14 +5,14 @@
  * struct dlistint_s - this is a struct for
  * a doubly linked list
  *
- * @data: this is our node
+ * @n: this is our node
  * @next: this is our pointer to next
  * @prev: this is our pointer to prev
  *
  */
 typedef struct dlistint_s
 {
-	int data;
+	int n;
 	struct dlistint_s *next;
 	struct dlistint_s *prev;
 } dlistint_t;
@@ -21,5 +21,6 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 
 #endif
